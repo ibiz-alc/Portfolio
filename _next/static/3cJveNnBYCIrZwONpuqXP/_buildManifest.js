9e8d9d357d6e26d0f6a6f1cd9964c11006076959
ibiz-alc <ibiz-alc@users.noreply.github.com>
2023-03-09 08:57:21 +0000

    deploy: ef06c93bb6802b9dac33c2322b57d0db1a86d0af

---
+++ _next/static/3cJveNnBYCIrZwONpuqXP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a1008abae78cd5c6.css","static/chunks/pages/index-490277ee41a145f8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
